a67cf26dc90086c727a564abb4087d719ca1e340
Deepanshu Gurjar <deepanshu.gurjar@niser.ac.in>
2024-06-28 10:33:39 +0530

    bugfixes

--- application.py
+++ application.py
@@ -11,11 +11,11 @@ application=Flask(__name__)
 
 ## Route for a home page
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html') 
 
-@app.route('/predictdata',methods=['GET','POST'])
+@application.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
